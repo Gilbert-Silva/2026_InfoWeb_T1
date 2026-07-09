d58b89d5f71e0074888dc1f8f3ecb93bfc2e3cea
Gilbert-Silva <gilbertif1995@outlook.com>
2026-07-09 14:23:51 -0300

    Crud de serviço

--- Projeto/service.py
+++ Projeto/service.py
@@ -1,5 +1,7 @@
-from models.cliente import Cliente
-from models.clientedao import ClienteDAO
+from models.cliente import Cliente         # entidade
+from models.clientedao import ClienteDAO   # persistência
+from models.servico import Servico
+from models.servicodao import ServicoDAO
 
 class Service:
     @staticmethod
@@ -20,3 +22,20 @@ class Service:
     def cliente_excluir(id):
         ClienteDAO().excluir(id)
     
+    @staticmethod
+    def servico_inserir(id, descricao, valor):
+        obj = Servico(id, descricao, valor)
+        ServicoDAO().inserir(obj)
+    @staticmethod
+    def servico_listar():
+        return ServicoDAO().listar()
+    @staticmethod
+    def servico_listar_id(id):
+        return ServicoDAO().listar_id(id)
+    @staticmethod
+    def servico_atualizar(id, descricao, valor):
+        obj = Servico(id, descricao, valor)
+        ServicoDAO().atualizar(obj)
+    @staticmethod
+    def servico_excluir(id):
+        ServicoDAO().excluir(id)
